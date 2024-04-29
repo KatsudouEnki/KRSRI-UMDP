532d3eaac34b1b7759470a5691a165ea22dd62e8
Brrrrrian <brian124brilliant@gmail.com>
2024-04-29 15:46:33 +0700

    ladder jalan sudah ok

--- krsri24/WalkTestV4/WalkCommand.ino
+++ krsri24/WalkTestV4/WalkCommand.ino
@@ -19,13 +19,16 @@
 */
 
 void loop(){
-  
+  // while(true){
+  //   Serial.println(digitalRead(11));
+  // }
+
   preparation();
   display.clearDisplay();
   servoAngkat.write(0);
   delay(5000);
 
-  speed = 100;
+  speed = 150;
 
 //  obstacle_kelereng();
 //  digitalWrite(7,HIGH);
@@ -35,51 +38,76 @@ void loop(){
 //    distance_detection();
 //    data_display();
   
-  while(true){ladder();}
-  // while(true){
-  // LeftFront(8,6,2.5,speed,servo_delay);
-  // delay(400);
-  // LeftFront(8,10,2.5,speed,servo_delay);
-  // delay(400);
-  // LeftFront(8,10,7,speed,servo_delay);
-  // delay(400);
-  // RightFront(8,6,2.5,speed,servo_delay);
-  // delay(400);
-  // RightFront(8,10,2.5,speed,servo_delay);
-  // delay(400);
-  // RightFront(8,10,7,speed,servo_delay);
-  // delay(400);
+  // while(true){ladder();}
+  while(true){
+  LeftFront(4.55,14.3,3.25,speed,servo_delay);
+  RightFront(4.55,14.3,3.25,speed,servo_delay);
+  LeftMid(6,10,5.5,speed,servo_delay);
+  RightMid(6,10,5.5,speed,servo_delay);
+  LeftBack(9.9,3.15,7.5,speed,servo_delay);
+  RightBack(9.9,3.15,7.5,speed,servo_delay);
+  delay(500);
 
-//  LeftMid(8,5,2.5,speed,servo_delay);
-//  RightMid(8,5,2.5,speed,servo_delay);
-//  delay(400);
-//  LeftMid(5,8,2.5,speed,servo_delay);
-//  RightMid(5,8,2.5,speed,servo_delay);
-//  delay(400);
-//  LeftMid(5,8,7,speed,servo_delay);
-//  RightMid(5,8,7,speed,servo_delay);
-//  delay(400);
-//
-//  LeftBack(10,8,2.5,speed,servo_delay);
-//  RightBack(10,8,2.5,speed,servo_delay);
-//  delay(400);
-//  LeftBack(6,8,2.5,speed,servo_delay);
-//  RightBack(6,8,2.5,speed,servo_delay);
-//  delay(400);
-//  LeftBack(6,8,7,speed,servo_delay);
-//  RightBack(6,8,7,speed,servo_delay);
-//  delay(400);
-//
-//  LeftFront(8,6,7,speed,servo_delay);
-//  RightFront(8,6,7,speed,servo_delay);
-//  
-//  LeftMid(8,5,7,speed,servo_delay);
-//  RightMid(8,5,7,speed,servo_delay);
-//
-//  LeftBack(10,8,7,speed,servo_delay);
-//  RightBack(10,8,7,speed,servo_delay);
-//
-//  delay(500);
+  LeftFront(2.8,8.8,3.25,speed,servo_delay);
+  RightFront(2.8,8.8,3.25,speed,servo_delay);
+  LeftMid(10,6,5.5,speed,servo_delay);
+  RightMid(10,6,5.5,speed,servo_delay);
+  LeftBack(14.3,4.55,7.5,speed,servo_delay);
+  RightBack(14.3,4.55,7.5,speed,servo_delay);
+  delay(750);
+
+  LeftFront(2.8,8.8,0.6,speed,servo_delay);
+  RightBack(14.3,4.55,5,speed,servo_delay);
+  delay(100);
+    LeftFront(3.15,9.9,0.6,speed,servo_delay);
+    delay(150);
+    LeftFront(3.5,11,0.6,speed,servo_delay);
+    delay(150);
+    LeftFront(3.85,12.1,0.6,speed,servo_delay);
+    RightBack(11,3.5,5,speed,servo_delay);
+    delay(150);
+    LeftFront(4.2,13.2,0.6,speed,servo_delay);
+    delay(150);
+    LeftFront(4.55,14.3,0.6,speed,servo_delay);
+    delay(150);
+  LeftFront(4.9,15.4,0.6,speed,servo_delay);
+  RightBack(9.9,3.15,5,speed,servo_delay);
+  delay(150);
+  LeftFront(4.55,14.3,3.25,speed,servo_delay);
+  RightBack(9.9,3.15,7.5,speed,servo_delay);
+  delay(400);
+
+  RightFront(2.8,8.8,0.6,speed,servo_delay);
+  LeftBack(14.3,4.55,5,speed,servo_delay);
+  delay(100);
+    RightFront(3.15,9.9,0.6,speed,servo_delay);
+    delay(150);
+    RightFront(3.5,11,0.6,speed,servo_delay);
+    delay(150);
+    RightFront(3.85,12.1,0.6,speed,servo_delay);
+    LeftBack(11,3.5,5,speed,servo_delay);
+    delay(150);
+    RightFront(4.2,13.2,0.6,speed,servo_delay);
+    delay(150);
+    RightFront(4.55,14.3,0.6,speed,servo_delay);
+    delay(150);
+  RightFront(4.9,15.4,0.6,speed,servo_delay);
+  LeftBack(9.9,3.15,5,speed,servo_delay);
+  delay(150);
+  RightFront(4.55,14.3,3.25,speed,servo_delay);
+  LeftBack(9.9,3.15,7.5,speed,servo_delay);
+  delay(400);
+
+  LeftMid(10,6,3.5,speed,servo_delay);
+  RightMid(10,6,3.5,speed,servo_delay);
+  delay(200);
+  LeftMid(6,10,3.5,speed,servo_delay);
+  RightMid(6,10,3.5,speed,servo_delay);
+  delay(200);
+  LeftMid(6,10,5.5,speed,servo_delay);
+  RightMid(6,10,5.5,speed,servo_delay);
+  delay(200);
+  }
 
   digitalWrite(7, LOW);
   default_state();
